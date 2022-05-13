14774cf23721724ca2e3828492bba8e718fd58cf
VAL1SOR <85311667+VAL1SOR@users.noreply.github.com>
2022-05-13 22:02:14 +0300

    Delete import random.py

--- import random.py
+++
@@ -1,7 +0,0 @@
-import random 
-import string
-
-def get_random_string(length):
-    return ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(length))
-
-print(get_random_string(20))
