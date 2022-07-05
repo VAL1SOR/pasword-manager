35fc23fa42b58c2f13c645ee99b8a962873cc457
VAL1SOR <85311667+VAL1SOR@users.noreply.github.com>
2022-07-05 10:14:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,66 @@
-from asyncio.windows_events import NULL
 import random 
 import string
 from tkinter import *
 from tkinter import Tk
-from webbrowser import get
 
 root = Tk()
+root.title("Password Generator")
+root.geometry("400x200")
 
-v = NULL
+v = IntVar()
+t = IntVar()
+c = IntVar()
+r = IntVar()
+a = 1
 
-t = NULL
+c1 = Checkbutton(root, text='lowercase',variable = t, onvalue = random.choice(string.ascii_lowercase), offvalue = 0)
+c2 = Checkbutton(root, text='uppercase',variable = v, onvalue = random.choice(string.ascii_uppercase), offvalue = 0)
+c3 = Checkbutton(root, text='digits',variable = c, onvalue = random.choice(string.digits), offvalue = 0)
+c4 = Checkbutton(root, text='punctuation',variable = r, onvalue = random.choice(string.punctuation), offvalue = 0)
+z = Spinbox(root, from_=1, to=2048, textvariable = a)
 
-c = NULL
+c1.pack()
+c2.pack()
+c3.pack()
+c4.pack()
+z.pack()
 
-r = NULL
+lis = [t, v, c, r]
 
+if(t == 0 and v == 0 and c == 0 and r == 0):
+    print("Please select at least one character type")
 
+rr = []
 
-Checkbutton(root, text='lowercase',variable = t, onvalue = string.ascii_lowercase, offvalue = NULL).pack()
-Checkbutton(root, text='uppercase',variable = v, onvalue = string.ascii_uppercase, offvalue = NULL).pack()
-Checkbutton(root, text='digits',variable = c, onvalue = string.digits, offvalue = NULL).pack()
-Checkbutton(root, text='punctuation',variable = r, onvalue = string.punctuation, offvalue = NULL).pack()
-z = Spinbox(root, from_=1, to=2048, values = 5).pack()
 
 def get_random_string():
-    return ''.join(random.choice(t + v + c + r) for _ in range(z.get                    ))
+#    return ''.join(random.choice(rr) for _ in range(a)) 
+    result = ""
+    i = 0
+    while(i < a):
+        if(t != 0):
+            rr.append(string.ascii_lowercase)
+        if(v != 0):
+            rr.append(string.ascii_uppercase)
+
+        if(c != 0):
+            rr.append(string.digits)
+
+        if(r != 0):
+            rr.append(string.punctuation)
+
+
+        i += 1
+
+        str = "".join(random.choice(rr) for _ in range(a))
+
+    if(i >= a):
+        for _ in range(a):
+            result = "".join(random.choice(str) for _ in range(a))
+            fresult = "".join(random.choice(result) for _ in range(a))
+            
+
+    print(fresult)
 
 Button(root, text = "Generate", command = get_random_string).pack()
 
